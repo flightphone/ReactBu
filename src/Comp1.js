40eb00690f2a59795691d5f5b810a123e0e5ca2f
Efim Manevich <efimmanevich@gmail.com>
2020-07-21 16:59:11 +0300

    scroll

--- src/Comp1.js
+++ src/Comp1.js
@@ -39,130 +39,137 @@ const useStyles = makeStyles((theme) => ({
   menuButton: {
     marginRight: theme.spacing(2),
   },
+  offset: theme.mixins.toolbar,
   title: {
     flexGrow: 1,
   },
+  fixheight: {
+    height: "100vh",
+    overflow: "auto",
+  },
 }));
 
-function GetComp(props)
-{
-    const classes = useStyles();
-    const current = props.component;
-    const Res = listcm[current];
-    //const [id, SetId] = useState(props.id);
-    let id = props.id;
-    return <Res id={id}/> ;
+function GetComp(props) {
+  const classes = useStyles();
+  const current = props.component;
+  const Res = listcm[current];
+  //const [id, SetId] = useState(props.id);
+  let id = props.id;
+  return <Res id={id} />;
 }
 
-function Comp1(props)
-{
-    const visible = props.visible;
-    const classes = useStyles();
-    return <div
+function Comp1(props) {
+  const visible = props.visible;
+  const classes = useStyles();
+  return <div
     hidden={!visible}
-    >
-<div className={classes.root}>
-          <AppBar position="static">
-            <Toolbar>
-              <Tooltip title="Меню">
-              <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu" onClick={() => { props.show();}}>
-                <MenuIcon />
-              </IconButton>
-              </Tooltip>
-              <Typography variant="h6" className={classes.title}>
-                Карточки рейса
+  >
+    <div className={classes.root}>
+      <AppBar position="fixed">
+        <Toolbar>
+          <Tooltip title="Меню">
+            <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu" onClick={() => { props.show(); }}>
+              <MenuIcon />
+            </IconButton>
+          </Tooltip>
+          <Typography variant="h6" className={classes.title}>
+            Карточки рейса
               </Typography>
-            </Toolbar>
-          </AppBar>
-        </div>
+        </Toolbar>
+      </AppBar>
+      <div className={classes.offset} />
     </div>
+  </div>
 }
 
-function Comp(props)
-{
+function Comp(props) {
 
-    
+
+  /*
+  if (MainList.length == 0)
+  {
+      let f2 = new Fin();
+      f2.items = ["uu1", "zz1", "xx1"];
+      f2.text = "привет";
+      MainList.push(f2);
+  };
+  */
+  const classes = useStyles();
+  const { id, visible } = props;
+  const p = MainList[id];
+
+  //const p = new Fin();
+  //p.items = ["aa", "bb", "cc"];
+
+  const [items, setItems] = useState(p.items);
+  const [txt, setTxt] = useState(p.text);
+
+
+
+  function handleChange(event) {
+    var a = event.target.value;
+    setTxt(a);
+    p.text = a;
+  }
+
+  function updateTab() {
+    p.updateTab();
+    let a = [];
+    p.items.forEach(element => {
+      a.push(element);
+    });
+    setItems(a);
     /*
-    if (MainList.length == 0)
-    {
-        let f2 = new Fin();
-        f2.items = ["uu1", "zz1", "xx1"];
-        f2.text = "привет";
-        MainList.push(f2);
-    };
+        <Button onClick={() => { Setcurrent(0); }} >Дополнительные тарифы</Button> <br />
+          <Button onClick={() => { Setcurrent(1); }} >Номера ВС</Button> <br />
+          <Button onClick={() => { Setcurrent(2); }} >Тип ВС</Button> <br />
+
     */
-    const classes = useStyles();
-    const {id, visible } = props;
-    const p  = MainList[id];
-    
-    //const p = new Fin();
-    //p.items = ["aa", "bb", "cc"];
-    
-    const [items, setItems] = useState(p.items);
-    const [txt, setTxt] = useState(p.text);
 
-    
+  }
+  return (
+    <div hidden={!visible} className={classes.fixheight}>
 
-    function handleChange(event) {
-      var a = event.target.value; 
-      setTxt(a);
-      p.text = a;
-    }
-
-    function updateTab()
-    {
-      p.updateTab();
-      let a = []; 
-      p.items.forEach(element => {
-        a.push(element);
-      });
-      setItems(a);
-      /*
-          <Button onClick={() => { Setcurrent(0); }} >Дополнительные тарифы</Button> <br />
-            <Button onClick={() => { Setcurrent(1); }} >Номера ВС</Button> <br />
-            <Button onClick={() => { Setcurrent(2); }} >Тип ВС</Button> <br />
-
-      */      
-      
-    }
-    return (
-    <div hidden={!visible}>
-
-<div className={classes.root}>
-          <AppBar position="static">
-            <Toolbar>
+      <div className={classes.root}>
+        <AppBar position="fixed">
+          <Toolbar>
             <Tooltip title="Меню">
-              <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu" onClick={() => { props.show();}}>
+              <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu" onClick={() => { props.show(); }}>
                 <MenuIcon />
               </IconButton>
-              </Tooltip>
-              <Typography variant="h6" className={classes.title}>
-                Редактирование записи
-              </Typography>
-              <Button color="inherit">Login</Button>
-            </Toolbar>
-          </AppBar>
-        </div>
+            </Tooltip>
+            <Typography variant="h6" className={classes.title}>
+              Редактирование записи
+            </Typography>
 
-
-      <div>
-        <span>{id}</span>
-        <input type="text" value = {txt} onChange={handleChange}/> 
+          </Toolbar>
+        </AppBar>
       </div>
-      <Tooltip title="Добавить">
-      <Button onClick={() => updateTab()}>Update</Button>
-      </Tooltip>
-      {
-        items.map ((it)=>(
-          <div>
+
+
+      <div >
+        <div className={classes.offset} />
+
+        <div>
+          <span>{id}</span>
+          <input type="text" value={txt} onChange={handleChange} />
+        </div>
+        <Tooltip title="Добавить">
+          <Button onClick={() => updateTab()}>Update</Button>
+        </Tooltip>
+        {
+          items.map((it) => (
+            <div>
               <span>{it}</span>
               <input type="text" />
-          </div>
+            </div>
           ))
-      }
+        }
+      </div>
+
     </div>
-    );
-        
+  );
+
 }
-export {Comp1, GetComp}
+export { Comp1, GetComp }
 export default Comp;
