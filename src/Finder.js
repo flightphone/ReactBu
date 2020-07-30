adfe8840601e04b68283c1e0e9dc35fec4fa108a
Efim Manevich <efimmanevich@gmail.com>
2020-07-30 10:44:43 +0300

    разбивка на страницы, верстка

--- src/Finder.js
+++ src/Finder.js
@@ -3,17 +3,33 @@ import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
-import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
 import Tooltip from '@material-ui/core/Tooltip';
+import Drawer from '@material-ui/core/Drawer';
+
+
 import { baseUrl, openMap } from './App';
 import DataGrid from './DataGrid';
+
 import MenuIcon from '@material-ui/icons/Menu';
 import FilterListIcon from '@material-ui/icons/FilterList';
+import CodeIcon from '@material-ui/icons/Code';
+import FirstPageIcon from '@material-ui/icons/FirstPage';
+import KeyboardArrowLeft from '@material-ui/icons/KeyboardArrowLeft';
+import KeyboardArrowRight from '@material-ui/icons/KeyboardArrowRight';
+import LastPageIcon from '@material-ui/icons/LastPage';
+
+
 
 const useStyles = makeStyles((theme) => ({
     root: {
         flexGrow: 1,
+        marginLeft: theme.spacing(2.5),
+    },
+    pag: {
+        flexGrow: 1,
+        marginLeft: theme.spacing(2.5),
+        alignContent: 'right'
     },
     menuButton: {
         marginRight: theme.spacing(2),
@@ -30,10 +46,18 @@ const useStyles = makeStyles((theme) => ({
 
 
 function Finder(props) {
+    const classes = useStyles();
+
+
     const [Descr, setDescr] = useState("Загрузка...");
     const [load, setLoad] = useState(true);
+    const [stateDrawer, setStateDrawer] = useState(false);
+    const [page, setPage] = useState(0);
+    const [count, setCount] = useState(200);
+
+
+
     const visible = props.visible;
-    const classes = useStyles();
     const id = props.id;
     const IdDeclare = props.params;
 
@@ -57,6 +81,7 @@ function Finder(props) {
             let v = openMap.get(id);
             v.data = data;
             setDescr(data.Descr);
+            setCount(data.TotalTab[0].n_total);
             setLoad(false);
         }
     }
@@ -66,30 +91,104 @@ function Finder(props) {
         //return <div>{openMap.get(id).data.TotalTab[0].n_total}</div>
     }
 
-    return <div
-        hidden={!visible}
-        className={classes.fixheight}
-    >
-        <AppBar position="fixed">
-            <Toolbar>
-                <Tooltip title="Меню">
-                    <IconButton onClick={() => { props.show(); }}>
-                        <MenuIcon />
-                    </IconButton>
-                </Tooltip>
-                <Typography variant="h6" className={classes.title}>
-                    {Descr}
+
+
+
+    const toggleDrawer = (open) => (event) => {
+
+        setStateDrawer(open);
+
+    };
+
+    const rowsPerPage = 30;
+    
+    function onChangePage(event, p)
+    {
+        setPage(p);
+        setStateDrawer(false);
+    }
+
+    const handleFirstPageButtonClick = (event) => {
+        onChangePage(event, 0);
+      };
+    
+      const handleBackButtonClick = (event) => {
+        onChangePage(event, page - 1);
+      };
+    
+      const handleNextButtonClick = (event) => {
+        onChangePage(event, page + 1);
+      };
+    
+      const handleLastPageButtonClick = (event) => {
+        onChangePage(event, Math.max(0, Math.ceil(count / rowsPerPage) - 1));
+      };
+
+
+    return (
+    <React.Fragment>
+        <Drawer anchor="top" open={stateDrawer} onClose={toggleDrawer(false)}>
+        <Toolbar>
+                <Typography className={classes.title}>
+                    {page * rowsPerPage + 1} - {Math.min((page+1)*rowsPerPage, count)} из {count}
                 </Typography>
-                <Tooltip title="Фильтровка и сортировка">
-                    <IconButton aria-label="filter list">
-                        <FilterListIcon />
-                    </IconButton>
-                </Tooltip>
+                <IconButton
+                    onClick={handleFirstPageButtonClick}
+                    disabled={page === 0}
+                    aria-label="first page"
+                >
+                    <FirstPageIcon />
+                </IconButton>
+                <IconButton onClick={handleBackButtonClick} disabled={page === 0} aria-label="previous page">
+                    <KeyboardArrowLeft />
+                </IconButton>
+                {page + 1} из {Math.max(0, Math.ceil(count / rowsPerPage) - 1) + 1}
+                <IconButton
+                    onClick={handleNextButtonClick}
+                    disabled={page >= Math.ceil(count / rowsPerPage) - 1}
+                    aria-label="next page"
+                >
+                    <KeyboardArrowRight />
+                </IconButton>
+                <IconButton
+                    onClick={handleLastPageButtonClick}
+                    disabled={page >= Math.ceil(count / rowsPerPage) - 1}
+                    aria-label="last page"
+                >
+                    <LastPageIcon />
+                </IconButton>
             </Toolbar>
-        </AppBar>
-        <div className={classes.offset} />
-        {(load) ? <span></span> : renderTab()}
-    </div>
+        </Drawer>
+        <div
+            hidden={!visible}
+            className={classes.fixheight}
+        >
+            <AppBar position="fixed">
+                <Toolbar>
+                    <Tooltip title="Меню">
+                        <IconButton onClick={() => { props.show(); }}>
+                            <MenuIcon />
+                        </IconButton>
+                    </Tooltip>
+                    <Typography variant="h6" className={classes.title}>
+                        {Descr}
+                    </Typography>
+                    <Tooltip title="Фильтровка и сортировка">
+                        <IconButton aria-label="filter list">
+                            <FilterListIcon />
+                        </IconButton>
+                    </Tooltip>
+                    <Tooltip title="Страницы">
+                        <IconButton onClick={() => { setStateDrawer(true); }}>
+                            <CodeIcon />
+                        </IconButton>
+                    </Tooltip>
+                </Toolbar>
+            </AppBar>
+            <div className={classes.offset} />
+            {(load) ? <span></span> : renderTab()}
+        </div>
+    </React.Fragment>);
 }
 
 export default Finder;
